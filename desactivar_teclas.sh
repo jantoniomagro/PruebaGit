229fba7a8c8d94e30ae9e675c60c62679af6258a
Jose <jantoniomagro@users.noreply.github.com>
2019-01-09 13:05:10 +0100

    Update desactivar_teclas.sh

--- desactivar_teclas.sh
+++ desactivar_teclas.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-xmodmap -e ‘keycode 96=’ #F12
-xmodmap -e ‘keycode 68=’ #F2
-xmodmap -e ‘keycode 69=’ #F3
-xmodmap -e ‘keycode 75=’ #F9
-xmodmap -e ‘keycode 95=’ #F11
-xmodmap -e ‘keycode 37=’ #Ctrl
-xmodmap -e ‘keycode 64=’ #Alt
-xmodmap -e ‘keycode 133=’ #Windows
+xmodmap -e 'keycode 96=' #F12
+xmodmap -e 'keycode 68=' #F2
+xmodmap -e 'keycode 69=' #F3
+xmodmap -e 'keycode 75=' #F9
+xmodmap -e 'keycode 95=' #F11
+xmodmap -e 'keycode 37=' #Ctrl
+xmodmap -e 'keycode 64=' #Alt
+xmodmap -e 'keycode 133=' #Windows
